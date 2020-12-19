a2e625b05af75d4df4ac5e94b61adc8c9c9bf4a1
alexkharech <41755348+alexkharech@users.noreply.github.com>
2020-12-19 18:07:44 +0300

    Update checkOrder.ts

--- src/routes/checkOrder.ts
+++ src/routes/checkOrder.ts
@@ -5,5 +5,5 @@ import { testAgent, prodAgent } from '../shared/orangedata'
 export default asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
   const agent = req.query.isTest ? testAgent : prodAgent
   const status = await agent.getOrderStatus(req.body.inn, req.body.id)
-  res.send({ success: !!status })
+  res.send({ status })
 })
